6288db6cbeba7f499e5bed238582836e1b1fb41d
ilgulee <iglee2000ca@gmail.com>
2016-12-15 19:27:05 -0500

    Add project files.

---
+++ CRUDSqlServe/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace CRUDSqlServe
+{
+    partial class Crud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addressTxtBox = new System.Windows.Forms.TextBox();
+            this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.foxLearnCrudDataSet = new CRUDSqlServe.FoxLearnCrudDataSet();
+            this.EmailTxtBox = new System.Windows.Forms.TextBox();
+            this.fullNametxtBox = new System.Windows.Forms.TextBox();
+            this.customerIdtxtBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTxtBox = new System.Windows.Forms.TextBox();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.customersTableAdapter = new CRUDSqlServe.FoxLearnCrudDataSetTableAdapters.CustomersTableAdapter();
+            this.customerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foxLearnCrudDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.addressTxtBox);
+            this.panel1.Controls.Add(this.EmailTxtBox);
+            this.panel1.Controls.Add(this.fullNametxtBox);
+            this.panel1.Controls.Add(this.customerIdtxtBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(26, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(844, 184);
+            this.panel1.TabIndex = 3;
+            // 
+            // addressTxtBox
+            // 
+            this.addressTxtBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customersBindingSource, "Address", true));
+            this.addressTxtBox.Location = new System.Drawing.Point(137, 122);
+            this.addressTxtBox.Multiline = true;
+            this.addressTxtBox.Name = "addressTxtBox";
+            this.addressTxtBox.Size = new System.Drawing.Size(608, 46);
+            this.addressTxtBox.TabIndex = 3;
+            // 
+            // customersBindingSource
+            // 
+            this.customersBindingSource.DataMember = "Customers";
+            this.customersBindingSource.DataSource = this.foxLearnCrudDataSet;
+            // 
+            // foxLearnCrudDataSet
+            // 
+            this.foxLearnCrudDataSet.DataSetName = "FoxLearnCrudDataSet";
+            this.foxLearnCrudDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // EmailTxtBox
+            // 
+            this.EmailTxtBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customersBindingSource, "Email", true));
+            this.EmailTxtBox.Location = new System.Drawing.Point(137, 89);
+            this.EmailTxtBox.Name = "EmailTxtBox";
+            this.EmailTxtBox.Size = new System.Drawing.Size(608, 19);
+            this.EmailTxtBox.TabIndex = 2;
+            // 
+            // fullNametxtBox
+            // 
+            this.fullNametxtBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customersBindingSource, "FullName", true));
+            this.fullNametxtBox.Location = new System.Drawing.Point(137, 59);
+            this.fullNametxtBox.Name = "fullNametxtBox";
+            this.fullNametxtBox.Size = new System.Drawing.Size(608, 19);
+            this.fullNametxtBox.TabIndex = 1;
+            // 
+            // customerIdtxtBox
+            // 
+            this.customerIdtxtBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customersBindingSource, "CustomerID", true));
+            this.customerIdtxtBox.Location = new System.Drawing.Point(137, 20);
+            this.customerIdtxtBox.Name = "customerIdtxtBox";
+            this.customerIdtxtBox.Size = new System.Drawing.Size(155, 19);
+            this.customerIdtxtBox.TabIndex = 0;
+            this.customerIdtxtBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Full Name:";
+            // 
+            // searchTxtBox
+            // 
+            this.searchTxtBox.Location = new System.Drawing.Point(163, 228);
+            this.searchTxtBox.Name = "searchTxtBox";
+            this.searchTxtBox.Size = new System.Drawing.Size(608, 19);
+            this.searchTxtBox.TabIndex = 2;
+            this.searchTxtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.searchTxtBox_KeyPress);
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Location = new System.Drawing.Point(388, 509);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(75, 23);
+            this.insertBtn.TabIndex = 1;
+            this.insertBtn.Text = "New Insert";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customerIDDataGridViewTextBoxColumn,
+            this.fullNameDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.customersBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(26, 278);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 21;
+            this.dataGridView.Size = new System.Drawing.Size(844, 191);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Search:";
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(499, 509);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(75, 23);
+            this.editBtn.TabIndex = 2;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            this.editBtn.Click += new System.EventHandler(this.editBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(622, 509);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 3;
+            this.cancelBtn.Text = "Canceel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(738, 509);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // customersTableAdapter
+            // 
+            this.customersTableAdapter.ClearBeforeFill = true;
+            // 
+            // customerIDDataGridViewTextBoxColumn
+            // 
+            this.customerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID";
+            this.customerIDDataGridViewTextBoxColumn.Name = "customerIDDataGridViewTextBoxColumn";
+            this.customerIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fullNameDataGridViewTextBoxColumn
+            // 
+            this.fullNameDataGridViewTextBoxColumn.DataPropertyName = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.HeaderText = "FullName";
+            this.fullNameDataGridViewTextBoxColumn.Name = "fullNameDataGridViewTextBoxColumn";
+            this.fullNameDataGridViewTextBoxColumn.Width = 180;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // Crud
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 579);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.searchTxtBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "Crud";
+            this.Text = "Database CRUD";
+            this.Load += new System.EventHandler(this.crud_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foxLearnCrudDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox addressTxtBox;
+        private System.Windows.Forms.TextBox EmailTxtBox;
+        private System.Windows.Forms.TextBox fullNametxtBox;
+        private System.Windows.Forms.TextBox customerIdtxtBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox searchTxtBox;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private FoxLearnCrudDataSet foxLearnCrudDataSet;
+        private System.Windows.Forms.BindingSource customersBindingSource;
+        private FoxLearnCrudDataSetTableAdapters.CustomersTableAdapter customersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ CRUDSqlServe/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDSqlServe
+{
+    public partial class Crud : Form
+    {
+        public Crud()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crud_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'foxLearnCrudDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.foxLearnCrudDataSet.Customers);
+            customersBindingSource.DataSource = this.foxLearnCrudDataSet.Customers;
+
+        }
+
+        private void searchTxtBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel1.Enabled = true;
+                fullNametxtBox.Focus();
+                this.foxLearnCrudDataSet.Customers.AddCustomersRow(this.foxLearnCrudDataSet.Customers.NewCustomersRow());
+                customersBindingSource.MoveLast();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = true;
+            fullNametxtBox.Focus();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            customersBindingSource.ResetBindings(false);
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                customersBindingSource.EndEdit();
+                customersTableAdapter.Update(this.foxLearnCrudDataSet.Customers);
+                panel1.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
